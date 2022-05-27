05ab4c2bcea2692d717514ceee4099f8878c1431
LordMalto <101904063+LordMalto@users.noreply.github.com>
2022-05-27 16:23:36 +0200

    Input change: Integer instead of String

--- Password-Generator.py
+++ Password-Generator.py
@@ -11,28 +11,27 @@ used_alphabets = letters_lowercase + letters_uppercase + numbers + special_chara
 
 print('Choose your password length: \n')
 
-password_length = input()
-password_length_int = int(password_length)
+password_length = int(input())
+
 
 print('Choose the number of passwords: \n')
 
-password_count = input()
-password_count_int = int(password_count)
-
-
+password_count = int(input())
 
 print('')
 
-if password_count_int == 1:
+if password_count == 1:
     print('>>>Your password is<<<')
+elif password_count == 0:
+    print('>>>No password<<<')
 else:
     print('>>>Your passwords are<<<')
 
 print('')
 
-for j in range(password_count_int):
+for j in range(password_count):
         password = ''.join(secrets.choice(used_alphabets)
-            for i in range(password_length_int))
+            for i in range(password_length))
         print(password)
 
 print('')
